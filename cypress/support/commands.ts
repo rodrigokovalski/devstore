2d5474cc75da87a5b07129454f85cf0bbb159c61
rodrigokovalski <rodrigokvk@gmail.com>
2025-03-23 10:26:38 -0300

    fix:

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -35,3 +35,14 @@
 //     }
 //   }
 // }
+
+declare namespace Cypress {
+    interface Chainable {
+        searchByQuery(query: string): Chainable<void>
+    }
+}
+
+Cypress.Commands.add('searchByQuery', (query: string) => {
+    cy.visit('/')
+    cy.get('input[name=q]').type(query).parent('form').submit()
+})
