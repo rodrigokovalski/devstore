bf549cb8429b35586fc9ad2780ef28eeea594af3
rodrigokovalski <rodrigokvk@gmail.com>
2025-03-23 11:10:56 -0300

    fix: Ajusta suspense de loading com useSearchParams

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,12 +1,14 @@
 import { Skeleton } from '@/components/skeleton'
+import { Suspense } from 'react'
+import { CurrentSearch } from './current-search'
 
 export default function SearchLoading() {
 
     return (
         <div className="flex flex-col gap-4">
-            <p className="text-sm">
-                Resultados para: <span className="font-semibold"></span>
-            </p>
+            <Suspense fallback={null}>
+                <CurrentSearch />
+            </Suspense>
 
             <div className="grid grid-cols-3 gap-6">
                 <Skeleton className="h-[400px]" />
