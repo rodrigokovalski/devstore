f36dc16bdce55b4779b2ec87f62d696d896e8c56
rodrigokovalski <rodrigokvk@gmail.com>
2025-03-23 10:48:20 -0300

    fix: ajustes next 15 and above featurees

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,17 +1,11 @@
-'use client'
-
 import { Skeleton } from '@/components/skeleton'
-import { useSearchParams } from 'next/navigation'
 
 export default function SearchLoading() {
-    const searchParams = useSearchParams()
-
-    const query = searchParams.get('q')
 
     return (
         <div className="flex flex-col gap-4">
             <p className="text-sm">
-                Resultados para: <span className="font-semibold">{query}</span>
+                Resultados para: <span className="font-semibold"></span>
             </p>
 
             <div className="grid grid-cols-3 gap-6">
